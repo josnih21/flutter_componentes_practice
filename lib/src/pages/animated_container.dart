639b2bdd442bc6d964737ed1a0ac069dcac41545
José Ángel <josnih@gmail.com>
2020-05-17 02:22:50 +0100

    added animated container screen

--- lib/src/pages/animated_container.dart
+++ lib/src/pages/animated_container.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:components_practice/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 class AnimatedContainerPage extends StatefulWidget {
@@ -6,9 +9,51 @@ class AnimatedContainerPage extends StatefulWidget {
 }
 
 class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+
+  double _width = 50.0;
+  double _height = 50.0;
+  Color _color = Colors.cyan;
+
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8.0);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation Container Page'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          width: _width,
+          height: _height,
+          curve: Curves.fastOutSlowIn,
+          decoration: BoxDecoration(
+            borderRadius: _borderRadius,
+            color: _color
+          ),
+          duration: Duration(seconds: 1),         
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.play_arrow),
+        onPressed: (){
+          _changeState();
+        }
+        
+      ),
     );
+    
   }
-}
+
+  void _changeState() {
+    final random = Random();
+
+    setState((){
+      _width = random.nextInt(300).toDouble();
+      _height= random.nextInt(300).toDouble();
+      _color = Color.fromRGBO(random.nextInt(255), random.nextInt(255),random.nextInt(255), 1);
+      _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
+       //Navigator.of(context).pushNamed('/');
+    });
+  }
+}   
